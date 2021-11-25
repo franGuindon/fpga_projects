15eba4ccc4a68e861276c3927de712f53cf3ea61
franGuindon <fbadilla10@gmail.com>
2021-11-24 20:52:39 -0600

    utils/test_module: Add test module to test testbench writer
    This file next to a target test testbench file will be used
    to test the testbench writer.

---
+++ utils/test_module.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+module TestModule (
+    input single_input,
+    input [2:0] array_input,
+    output single_output,
+    output [2:0] array_output,
+    output reg single_reg_output,
+    output reg [2:0] reg_array_output,
+    inout single_inout,
+    inout [2:0] array_inout,
+    port_not_declared
+);
+    input port_not_declared;
+
+endmodule
