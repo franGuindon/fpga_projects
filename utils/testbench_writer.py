9ea340d3d6a56771125d0a1a26021f8516df0087
franGuindon <fbadilla10@gmail.com>
2021-11-28 11:29:09 -0600

    utils/testbench_writer: Refactor statement parsers into a single function

--- utils/testbench_writer.py
+++ utils/testbench_writer.py
@@ -39,119 +39,118 @@ def tokenize(string):
     )
     return tokens
 
-def parse_statements_from_delimiters(token_list, delimiters = ('','')):
-    start_delimiter, stop_delimiter = delimiters
-    statements = []
-    parsing_statement = []
+def parse_statements(token_list, start_delimiters, stop_delimiters, sep_delimiters=[]):
+    sep_delimiters = [*sep_delimiters, *stop_delimiters]
+
+    parsed_statements = []
+    statement_buffer = []
     remaining_tokens = []
     list_to_add_token = remaining_tokens
-    
+
     for token in token_list:
-        
-        if token == start_delimiter:
-            list_to_add_token = parsing_statement
-        
+
+        if token in start_delimiters:
+            list_to_add_token = statement_buffer
+
         list_to_add_token.append(token)
-        
-        if token == stop_delimiter:
+
+        if token in sep_delimiters:
+            parsed_statements.append(statement_buffer)
+            statement_buffer = []
+            list_to_add_token = statement_buffer
+
+        if token in stop_delimiters:
             list_to_add_token = remaining_tokens
-            statements.append(parsing_statement)
-            parsing_statement = []
 
-    statements = [statement for statement in statements if statement != []]
-    
-    return statements, remaining_tokens
+    parsed_statements = [
+        statement for statement in parsed_statements
+        if statement != [] and statement[0] not in sep_delimiters
+    ]
 
-def get_ports_from_port_statement(port_statement, direction='input'):
-    directions = ['input', 'output', 'inout']
-    directions.remove(direction)
-    remaining_directions = directions
-    ports = []
-    parsing_port = []
-    remaining_tokens = []
-    list_to_add_token = remaining_tokens
+    return parsed_statements, remaining_tokens
 
-    for token in port_statement:
+class TokenParser:
 
-        if token == direction:
-            list_to_add_token = parsing_port
-            continue
+    def __init__(s, token_list):
+        s.token_list = token_list
+        s.parse_comments()
+        s.parse_modules()
+        s.parse_module_sections()
+        s.parse_ports()
+
+    def parse_comments(s):
+        s.single_line_comments, s.token_list = parse_statements(
+            s.token_list,
+            start_delimiters = ['//'],
+            stop_delimiters = ['\n']
+        )
+        s.multiple_line_comments, s.token_list = parse_statements(
+            s.token_list,
+            start_delimiters = ['/*'],
+            stop_delimiters = ['*/']
+        )
+        s.token_list = [token for token in s.token_list if token != '\n']
+
+    def parse_modules(s):
+        s.modules, s.nonmodule_statements = parse_statements(
+            s.token_list,
+            start_delimiters = ['module'],
+            stop_delimiters = ['endmodule']
+        )
+        s.module_of_interest = s.modules[0]
         
-        if token in [',',')']:
-            ports.append(parsing_port)
-            parsing_port = []
-            continue
-        
-        if token in remaining_directions:
-            list_to_add_token = remaining_tokens
-        
-        list_to_add_token.append(token)
-    
-    ports = [port for port in ports if port != []]
+    def parse_module_sections(s):
+        s.module_declaration, s.module_body = parse_statements(
+            s.module_of_interest,
+            start_delimiters = ['module'],
+            stop_delimiters = [';']
+        )
+        s.module_declaration = s.module_declaration[0]
+        s.parameter_statement, s.module_declaration = parse_statements(
+            s.module_declaration,
+            start_delimiters = ['#'],
+            stop_delimiters = [')']
+        )
+        s.port_statement, s.module_declaration = parse_statements(
+            s.module_declaration,
+            start_delimiters = ['('],
+            stop_delimiters = [')']
+        )
+        s.port_statement = s.port_statement[0]
 
-    return ports
+    def parse_parameters(s):
+        get_parameters_from_parameter_statement(
 
-def parse_tokens(token_list):
-    class Namespace:
-        pass
-    parsed_tokens = Namespace()
-    p = parsed_tokens
+        )
 
-    p.single_line_comments, token_list = parse_statements_from_delimiters(
-        token_list, delimiters=('//','\n')
-    )
+    def parse_ports(s):
+        s.inputs_from_port_statement, _ = parse_statements(
+            s.port_statement,
+            start_delimiters = ['input'],
+            stop_delimiters = ['inout', 'output',')'],
+            sep_delimiters = [',']
+        )
 
-    token_list = [token for token in token_list if token != '\n']
+        s.outputs_from_port_statement, _ = parse_statements(
+            s.port_statement,
+            start_delimiters = ['output'],
+            stop_delimiters = ['input', 'inout',')'],
+            sep_delimiters = [',']
+        )
 
-    p.multiple_line_comments, token_list = parse_statements_from_delimiters(
-        token_list, delimiters=('/*','*/')
-    )
-
-    p.modules, p.nonmodule_statements = parse_statements_from_delimiters(
-        token_list, delimiters=('module','endmodule')
-    )
-
-    module_of_interest = p.modules[0]
-
-    module_declaration, p.module_body = parse_statements_from_delimiters(
-        module_of_interest, delimiters=('module',';')
-    )
-
-    module_declaration = module_declaration[0]
-
-    p.parameter_statement, module_declaration = parse_statements_from_delimiters(
-        module_declaration, delimiters=('#',')')
-    )
-
-    port_statement, p.module_declaration = parse_statements_from_delimiters(
-        module_declaration, delimiters=('(',')')
-    )
-
-
-    p.port_statement = port_statement[0]
-
-    p.inputs_from_port_statement = get_ports_from_port_statement(
-        p.port_statement, direction='input'
-    )
-
-    p.outputs_from_port_statement = get_ports_from_port_statement(
-        p.port_statement, direction='output'
-    )
-
-    p.inouts_from_port_statement = get_ports_from_port_statement(
-        p.port_statement, direction='inout'
-    )
-
-    return parsed_tokens
+        s.inouts_from_port_statement, _ = parse_statements(
+            s.port_statement,
+            start_delimiters = ['inout'],
+            stop_delimiters = ['input', 'output',')'],
+            sep_delimiters = [',']
+        )
 
 def main():
     with open(args.src) as file_obj:
         file_data = file_obj.read()
     
     token_list = tokenize(file_data)
-    parsed_tokens = parse_tokens(token_list)
-
-    print(parsed_tokens.inouts_from_port_statement)
+    parsed_tokens = TokenParser(token_list)
 
 if __name__ == '__main__':
     main()
